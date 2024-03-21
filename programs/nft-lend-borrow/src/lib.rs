b9be087f2d80b86ef259e25ed7e96f7a277b8c97
xabimolana <164369840+xabimolana@users.noreply.github.com>
2024-03-21 22:49:12 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("7zEJ1VgdqpggfNJmHs98pvbMyPAdGoV41ngYBwwP5Nn2");
 
 #[program]
 pub mod nft_lend_borrow {
